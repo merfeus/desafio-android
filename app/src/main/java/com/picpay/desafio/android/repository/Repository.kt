4a8695d5a5387e7e66a257c82d7e4322240e08d5
Mateus Morais <mateus658@hotmail.com>
2022-08-23 22:40:19 -0300

    feat: create insert and get functions from database

--- app/src/main/java/com/picpay/desafio/android/repository/Repository.kt
+++ app/src/main/java/com/picpay/desafio/android/repository/Repository.kt
@@ -6,4 +6,8 @@ import com.picpay.desafio.android.service.NetworkResponse
 interface Repository {
 
     suspend fun getUsers(): NetworkResponse<List<User>>
+
+    suspend fun getUsersFromDatabase(): List<User>?
+
+    suspend fun insertUsersFromDatabase(list: List<User>)
 }
