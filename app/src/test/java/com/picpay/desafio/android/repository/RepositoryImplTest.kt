a9614d070ba8f11a563e07966e10dac1e6c1fce9
Mateus Morais <mateus658@hotmail.com>
2022-08-23 23:03:15 -0300

    feat: create tests for repository

--- app/src/test/java/com/picpay/desafio/android/repository/RepositoryImplTest.kt
+++ app/src/test/java/com/picpay/desafio/android/repository/RepositoryImplTest.kt
@@ -1,5 +1,7 @@
 package com.picpay.desafio.android.repository
 
+import com.picpay.desafio.android.database.AppDatabase
+import com.picpay.desafio.android.database.dao.UsersDAO
 import com.picpay.desafio.android.model.User
 import com.picpay.desafio.android.service.PicPayService
 import io.mockk.coEvery
@@ -15,16 +17,42 @@ class RepositoryImplTest {
 
     private val apiService: PicPayService = mockk()
 
+    private val userDao: UsersDAO = mockk()
+    private val db: AppDatabase = mockk()
+
     @Test
     fun `when getUsers is called then it should call service getUsers`() {
         val user = User(img = "aaa", name = "Jose", id = 1, username = "Jose@aa")
         coEvery { apiService.getUsers() } returns Response.success(listOf(user))
 
         runBlockingTest {
-            RepositoryImpl(apiService).getUsers()
+            RepositoryImpl(apiService, userDao).getUsers()
         }
 
         coVerify { apiService.getUsers() }
+
+    }
+
+    @Test
+    fun `when getUsersFromDb is called then it should call users from db`() {
+        val user = User(id = 1, img = "aaa", name = "Jose", username = "Jose@aa")
+        coEvery { userDao.getAll() } returns listOf(user)
+
+        runBlockingTest {
+            RepositoryImpl(apiService, userDao).getUsersFromDatabase()
+        }
+
+        coVerify { userDao.getAll() }
+    }
+
+    @Test
+    fun `when insertUser is called then it should call insert in db`() {
+        val user = User(id = 2, img = "bbb", name = "Paulo", username = "Paulo@bbb")
+
+        runBlockingTest {
+            userDao.insert(listOf(user))
+        }
+        coVerify { userDao.insert(listOf(user)) }
 
     }
 }
